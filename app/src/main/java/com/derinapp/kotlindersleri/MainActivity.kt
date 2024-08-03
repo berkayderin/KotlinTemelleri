539bf32194eda6984f11323ee481a9621c30ddda
Berkay Derin <zewodi@gmail.com>
2024-08-03 14:35:58 +0300

    Nullability

--- app/src/main/java/com/derinapp/kotlindersleri/MainActivity.kt
+++ app/src/main/java/com/derinapp/kotlindersleri/MainActivity.kt
@@ -11,5 +11,35 @@ class MainActivity : AppCompatActivity() {
         // Superkahraman sınıfından bir nesne oluşturuyoruz.
         var superman = Superkahraman("Superman", 30, "Gazeteci")
         println("Superman'ın mesleği: ${superman.meslek}")
+
+        // Nullability
+        val kullaniciGirdisi = "atil"
+        val kullaciGirdisiInteger = kullaniciGirdisi.toIntOrNull() // uygulamanın çökmemesi için null olabilir
+
+        if (kullaciGirdisiInteger != null) {
+            println(kullaciGirdisiInteger * 2)
+            println("Kullanıcı girdisi integer'a dönüştürülebilir.")
+        } else {
+            println("Kullanıcı girdisi integer'a dönüştürülemez.")
+        }
+
+        val benimDouble: Double? // ? işareti ile null olabilir olduğunu belirttik
+        val kullaniciGirdisiDouble = kullaniciGirdisi.toDoubleOrNull() // uygulamanın çökmemesi için null olabilir
+
+        // !! => null olmayacak EMİNİM demektir => kullanımı önerilmez
+        kullaniciGirdisiDouble!!.plus(5.0) // null olmayacak emin olduğumuz için !! kullandık
+
+        // ? => safe call operatörü
+        println(kullaniciGirdisiDouble?.minus(3.0)) // null olabilir, olursa işlem yapma
+
+        // ?: => Elvis operatörü => null olursa varsayılan değeri ata
+        val sonuc = kullaniciGirdisiDouble?.minus(3.0) ?: 0.0 // null olursa 0.0 döndür
+
+        // let fonksiyonu
+        val kullaniciYasi = 30
+        kullaniciYasi.let {
+            println("Kullanıcı yaşını yazdırıyoruz: $it")
+        }
+
     }
 }
